e09256558d00afd23ceca6feb3d21d0deeed5337
Petr Morávek <petrmoravek6@gmail.com>
2023-01-23 19:48:51 +0100

    di?

--- client/clientUI/Program.cs
+++ client/clientUI/Program.cs
@@ -3,6 +3,10 @@ using clientUI.ServerApi;
 using clientUI.ServerApi.Model.Converter;
 using clientUI.Services;
 using clientUI.UIContext;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.Design;
+using System.IO;
 
 namespace clientUI;
 
@@ -20,27 +24,59 @@ static class Program
 
         string baseApiAddress = @"http://localhost:8080";
 
-        TeamConverter teamConverter = new TeamConverter();
+       //var host = Host.CreateDefaultBuilder()
+       //    .ConfigureServices((provider, services) =>
+       //    {
+       //        services.AddSingleton<TeamConverter>();
+       //        services.AddSingleton<TeamRequester>(provider => new TeamRequester(baseApiAddress, provider.GetService<TeamConverter>()!));
+       //        services.AddSingleton<PlayerRequester>(provider => new PlayerRequester(baseApiAddress, null, provider.GetService<TeamConverter>()!));
+       //        services.AddSingleton<MatchRequester>(provider => new MatchRequester(baseApiAddress, null));
+       //        services.AddSingleton<MatchConverter>();
+       //        services.AddSingleton<PlayerConverter>();
+       //
+       //        services.AddSingleton<TeamService>();
+       //        services.AddSingleton<PlayerService>();
+       //        services.AddSingleton<MatchService>();
+       //
+       //        services.AddSingleton<TeamContext>();
+       //        services.AddSingleton<PlayerContext>();
+       //        services.AddSingleton<MatchContext>();
+       //
+       //        services.AddSingleton<MainForm>();
+       //    })
+       //    .Build();
+       //
+       //var m = ServiceProvider.GetRequiredService<MainForm>();
+       //
+       //Application.Run(ServiceProvider.GetRequiredService<MainForm>());
 
-        TeamRequester teamRequester = new(baseApiAddress, teamConverter);
-        PlayerRequester playerRequester = new(baseApiAddress, null, teamConverter);
-        MatchRequester matchRequester = new(baseApiAddress, null);
+       
 
-        MatchConverter matchConverter = new MatchConverter(playerRequester, teamRequester);
-        PlayerConverter playerConverter = new PlayerConverter(teamRequester);
 
-        // setting cycle references
-        playerRequester.converter = playerConverter;
-        matchRequester.converter = matchConverter;
 
-        TeamService teamService = new(teamRequester);
-        PlayerService playerService = new(playerRequester);
-        MatchService matchService = new(matchRequester);
+        
 
-        TeamContext teamContext = new(teamService);
-        PlayerContext playerContext = new(playerService, teamService);
-        MatchContext matchContext = new(matchService, playerService, teamService);
-
-        Application.Run(new MainForm(teamContext, playerContext, matchContext));
+       TeamConverter teamConverter = new TeamConverter();
+       
+       TeamRequester teamRequester = new(baseApiAddress, teamConverter);
+       PlayerRequester playerRequester = new(baseApiAddress, null, teamConverter);
+       MatchRequester matchRequester = new(baseApiAddress, null);
+       
+       MatchConverter matchConverter = new MatchConverter(playerRequester, teamRequester);
+       PlayerConverter playerConverter = new PlayerConverter(teamRequester);
+       
+       // setting cycle references
+       playerRequester.converter = playerConverter;
+       matchRequester.converter = matchConverter;
+       
+       TeamService teamService = new(teamRequester);
+       PlayerService playerService = new(playerRequester);
+       MatchService matchService = new(matchRequester);
+       
+       TeamContext teamContext = new(teamService);
+       PlayerContext playerContext = new(playerService, teamService);
+       MatchContext matchContext = new(matchService, playerService, teamService);
+       
+       Application.Run(new MainForm(teamContext, playerContext, matchContext));
     }
 }
